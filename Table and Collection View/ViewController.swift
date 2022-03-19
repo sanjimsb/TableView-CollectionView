f2c4c929d3637737969f1424a682bc7e49842588
Bipin MSB <bpmsb45@gmail.com>
2022-03-19 11:53:21 -0400

    Initial Commit

---
+++ Table and Collection View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Table and Collection View
+//
+//  Created by Bipin Msb on 2022-03-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
